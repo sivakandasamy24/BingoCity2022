7c5a7c08b3d7bba9724d77234c16736f640d48b8
Sivasankar Kandasamy <SKandasamy@gsngames.com>
2022-09-08 21:07:07 +0530

    Adding BallRolling Count Adding Summary popup Adding AutoBuy popup

--- BingoCity_2022/Assets/Scripts/GameConfigData.cs
+++ BingoCity_2022/Assets/Scripts/GameConfigData.cs
@@ -6,12 +6,18 @@ namespace BingoCity
     [CreateAssetMenu(menuName = "GameConfigData", fileName = "GameConfigData", order = 0)]
     public class GameConfigData : ScriptableObject
     {
-        [SerializeField] private int maxNumberOfBallPerClick = 2;
         [SerializeField] private List<string> itemPattern;
+        [SerializeField] private int maxNumberOfBallPerClick = 2; //GS ball rollout on each click
+        [SerializeField] private int maxNumberRoll = 5; //GS max click on round starts
+        [SerializeField] private int buyPopupMaxWindowCount = 1; //GS additional click buy count
+        [SerializeField] private int buyPopupAutoTimer = 1; // GS autoTimerWaiting timerIn sec
+        [SerializeField] private int buyAdditionalRollCount = 2; //GS additional click buy count
 
+        public int MaxNumberRoll => maxNumberRoll;
+        public int BuyPopupMaxWindowCount => buyPopupMaxWindowCount;
+        public int BuyAdditionalRollCount => buyAdditionalRollCount;
         public List<string> ItemPattern => itemPattern;
         public int MaxNumberOfBallPerClick => maxNumberOfBallPerClick;
-        
-        
+        public int BuyPopupAutoTimer => buyPopupAutoTimer;
     }
 }
--- BingoCity_2022/Assets/Scripts/MainGame/UIManager.cs
+++ BingoCity_2022/Assets/Scripts/MainGame/UIManager.cs
@@ -1,26 +1,123 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace BingoCity
 {
     public class UIManager:MonoBehaviour
     {
         [SerializeField] private GameConfigData gameConfigData;
+        [SerializeField] private TextMeshProUGUI rollCountText;
+        [SerializeField] private Button rollButton;
 
+        [SerializeField] private GameObject popupParent;
+        [SerializeField] private GameObject buyRollPopup;
+        [SerializeField] private GameObject summaryPopup;
+
+
+        private int _currentRollCount;
+        private int _autoPopupShowCount;
         public GameConfigData GameConfigData => gameConfigData;
+
+        private void Start()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            _currentRollCount = gameConfigData.MaxNumberRoll;
+            _autoPopupShowCount = gameConfigData.BuyPopupMaxWindowCount;
+            popupParent.gameObject.SetActive(false);
+            buyRollPopup.gameObject.SetActive(false);
+            summaryPopup.gameObject.SetActive(false);
+            UpdateRollText();
+        }
+
+        private void UpdateRollText()
+        {
+            rollCountText.text = $" {_currentRollCount}";
+            CheckAndEnableRollButton();
+        }
+
+        private void CheckAndShowAutoPopup()
+        {
+            if (_autoPopupShowCount > 0)
+            {
+                //startTimerToShowBuyPopup
+                Invoke(nameof(ShowBuyRollCountPopup),gameConfigData.BuyPopupAutoTimer);
+            }
+            else
+            {
+                ShowGameSummaryPopup();
+            }
+        }
+
+        private void ShowGameSummaryPopup()
+        {
+            popupParent.gameObject.SetActive(true);
+            summaryPopup.SetActive(true);
+        }
+        private void ShowBuyRollCountPopup()
+        {
+            popupParent.gameObject.SetActive(true);
+            buyRollPopup.SetActive(true);
+        }
+
+        private void HideBuyRollCountPopup()
+        {
+            popupParent.gameObject.SetActive(false);
+            buyRollPopup.SetActive(false);
+        }
+        
+
+        private void CheckAndEnableRollButton()
+        {
+            if (_currentRollCount<1)
+            {
+                //disable Roll button
+                rollButton.interactable = false;
+                CheckAndShowAutoPopup();
+            }
+            else
+            {
+                rollButton.interactable = true;
+            }
+        }
+
         public void OnResetGameButton()
         {
+            InitData();
             EventManager.OnRestartGameButtonEvent();
         }
 
         public void OnGetNextBallButton()
         {
+            _currentRollCount--;
+            UpdateRollText();
+            
             EventManager.OnGetNextBallButtonEvent();
         }
 
         public void OnBackButton(int loadSceneIndex)
         {
             SceneManager.LoadScene(loadSceneIndex);
+        }
+
+        public void OnBuyPopupYes(bool isBuying)
+        {
+            HideBuyRollCountPopup();
+            if (isBuying)
+            {
+                _autoPopupShowCount--;
+                _currentRollCount += gameConfigData.BuyAdditionalRollCount;
+                UpdateRollText();
+            }
+            else
+            {
+                ShowGameSummaryPopup();
+            }
         }
     }
 }
